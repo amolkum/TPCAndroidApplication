bb06a8e0c3016e1129fcb216f9d67cf09ea17d4d
Amol Kumbhar <amolkum143@gmail.com>
2018-05-03 19:11:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/goodluck/tpcandroidapplication/TPCMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.goodluck.tpcandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TPCMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tpcmain);
+    }
+}
