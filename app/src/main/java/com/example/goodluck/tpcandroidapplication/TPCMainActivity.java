5d4e38fa147b67befe006a492cc468a61c5684ae
Amol Kumbhar <amolkum143@gmail.com>
2018-05-03 20:40:55 +0530

    test

--- app/src/main/java/com/example/goodluck/tpcandroidapplication/TPCMainActivity.java
+++ app/src/main/java/com/example/goodluck/tpcandroidapplication/TPCMainActivity.java
@@ -1,7 +1,11 @@
 package com.example.goodluck.tpcandroidapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class TPCMainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class TPCMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tpcmain);
+
+        ImageButton imageButton=(ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(login);
+                Toast.makeText(getApplicationContext(), "Clicked Image Button",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
